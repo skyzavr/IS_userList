c96414a279b45905056d3ff388e178def4b53679
skyzavr <dankozero111@gmail.com>
2025-03-25 14:39:08 +0300

    [feat]: add global types

---
+++ src/shared/model/type.ts
@@ -0,0 +1,13 @@
+export type dataDesc = {
+  id: string;
+  name: string;
+  jobTitle: string;
+  department: string;
+  company: string;
+};
+
+export type IOoptions = {
+  root: null;
+  rootMargin: string;
+  threshold: number;
+};
