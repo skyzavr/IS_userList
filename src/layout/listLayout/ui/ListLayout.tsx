1e6b1a2d9852433f0c49feb4ffc237eccaf80d6b
skyzavr <dankozero111@gmail.com>
2025-03-25 15:45:22 +0300

    [fix]: fix list height depends on the screen height

--- src/layout/listLayout/ui/ListLayout.tsx
+++ src/layout/listLayout/ui/ListLayout.tsx
@@ -2,18 +2,25 @@ import { useSelector } from 'react-redux';
 
 import { UserList } from '@widgets/userList';
 import { usersSelector } from '@widgets/userList/model/selectors';
+import { EditPreview } from '@entities/editPreview';
 
 import css from './css.module.css';
 
 export const ListLayout = () => {
   const { userNumber } = useSelector(usersSelector);
+  const { innerWidth, innerHeight } = window;
+  const smallScreens = innerWidth < 840;
+
+  const fixGap = 250;
+  const height = smallScreens ? `${innerHeight - fixGap}px` : '100%';
 
   return (
     <section className={css.wrapper}>
+      {smallScreens && <EditPreview />}
       <div className={css.infoPanel}>
         <span>Количество пользователей - </span> <span>{userNumber}</span>
       </div>
-      <UserList />
+      <UserList height={height} />
     </section>
   );
 };
