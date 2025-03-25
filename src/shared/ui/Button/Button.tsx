03179fa08e7b652632fd097bf935aa4a9de7f94e
skyzavr <dankozero111@gmail.com>
2025-03-25 15:53:14 +0300

    [fix]: fix styles

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,14 +1,20 @@
+import classNames from 'classnames';
 import css from './button.module.css';
 type btnProps = {
   title: string;
   onUpdate: () => void;
   isDisabled: boolean;
+  btnType?: 'fill' | 'stroke';
 };
 export const Button = (props: btnProps) => {
-  const { title, isDisabled, onUpdate } = props;
+  const { title, isDisabled, onUpdate, btnType = 'fill' } = props;
 
   return (
-    <button disabled={isDisabled} className={css.wrapper} onClick={onUpdate}>
+    <button
+      disabled={isDisabled}
+      className={classNames(css.wrapper, css[btnType])}
+      onClick={onUpdate}
+    >
       {title}
     </button>
   );
