56ea87ebf9147351a2df36f862076e17716987b6
skyzavr <dankozero111@gmail.com>
2025-03-25 15:44:46 +0300

    [feat]: add responsive and editable area depense on width (popUp for small screens)

--- src/layout/editAreaLayout/ui/EditArea.tsx
+++ src/layout/editAreaLayout/ui/EditArea.tsx
@@ -3,11 +3,23 @@ import { useSelector } from 'react-redux';
 import { UserData } from '@widgets/userData';
 import { currentUserSelector } from '@widgets/userList/model/selectors';
 import { EditPreview } from '@entities/editPreview';
+import { createPortal } from 'react-dom';
+
+import css from './css.module.css';
 
 export const EditAreaLayout = () => {
+  const { innerWidth } = window;
   const { currentUser } = useSelector(currentUserSelector);
 
-  return (
-    <section>{currentUser.id !== '' ? <UserData /> : <EditPreview />}</section>
-  );
+  if (innerWidth >= 840)
+    return (
+      <section>
+        {currentUser.id !== '' ? <UserData /> : <EditPreview />}
+      </section>
+    );
+  if (currentUser.id !== '')
+    return createPortal(
+      <div className={css.modal}>{<UserData />}</div>,
+      document.body
+    );
 };
